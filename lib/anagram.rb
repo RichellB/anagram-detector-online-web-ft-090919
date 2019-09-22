7c9f8325dadbe31920e4daed168924c98d885e8f
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-22 16:04:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word_array) 
+    word_array.find_all do |w|
+      w.split("").sort == @word.split("").sort
+    end
+  end
+  
+end 
